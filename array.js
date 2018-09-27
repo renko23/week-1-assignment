c533c3260178c156de668b162d8dda9e7724d721
renko23 <43059394+renko23@users.noreply.github.com>
2018-09-27 01:28:52 -0700

    Add files via upload

---
+++ array.js
@@ -0,0 +1,27 @@
+//sum of arrays by using function//
+
+	// function sum(a){
+
+	// 	var sum = 0;
+
+	// 	var i;
+
+	// 	for(i=0; i<a.length; i++){
+	// 		sum = sum + a[i];
+	// 	}
+
+	// 	return sum;
+	// }
+     
+	// console.log('sum of the array of elements is:' + sum([6,7]));
+
+
+
+	//sum of arrays withhout using function//
+	var a =[2,4,6,8,10];
+	var sum = 0;
+	for(var i=0; i<a.length; i++){
+
+		sum = sum + a[i];
+	}
+   console.log('sum of the array of elements is:' +sum);
