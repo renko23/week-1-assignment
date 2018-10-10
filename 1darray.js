7b5f5f7006730ae0dadf6669f382c742299abe04
renko23 <43059394+renko23@users.noreply.github.com>
2018-10-10 12:47:08 +0530

    Add files via upload

---
+++ 1darray.js
@@ -0,0 +1,18 @@
+//for printing odd indexes
+// var arr1 = [1,733,34,22,8,93,22,2,0,4,5,6];
+// for(var i=0;i<arr1.length; i++){
+// 	if(i%2==1){
+// 		console.log(arr1[i])
+// 	}
+// }
+
+
+
+
+//for printing even numbers
+var arr1 = [1,56,89,456,789,32,46,78,560];
+for (var i=0; i<arr1.length; i++){
+	if(arr1[i]%2==0){
+		console.log(arr1[i])
+	}
+}
